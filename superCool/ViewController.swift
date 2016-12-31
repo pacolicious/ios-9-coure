5a79862d983e02b0eaea19de0e480bb1cfe44f1e
Sleek-Geek <nieto.paco@sleek-geek.net>
2016-12-31 13:44:37 -0800

    added a bunch of files

--- superCool/ViewController.swift
+++ superCool/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    @IBOutlet weak var img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func actionBtn(sender: AnyObject) {
+        
+        img.hidden = true
+        
+    }
 
 }
 
